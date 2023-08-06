efed6abdd2d6b2e56afa7a04a6a7db59fda9c089
Jullie Paixão <jlpaixaof@gmail.com>
2023-08-06 16:46:15 -0300

    Adicionando Mapper para Topic

---
+++ src/main/java/com/sicredi/cooperativismo/mapper/ITopicMapper.java
@@ -0,0 +1,15 @@
+package com.sicredi.cooperativismo.mapper;
+
+import com.sicredi.cooperativismo.domain.Topic;
+import com.sicredi.cooperativismo.dto.request.TopicRequest;
+
+public interface ITopicMapper {
+
+    static Topic buildTopic(TopicRequest topicRequest) {
+        return Topic.builder()
+                .title(topicRequest.title)
+                .startTime(topicRequest.startTime)
+                .endTime(topicRequest.endTime)
+                .build();
+    }
+}
