14accf85edcfbf2c9645e83962eb693442f6fcfd
Jullie <jlpaixaof@gmail.com>
2023-11-24 01:15:24 -0300

    feat: adding new elements to Topic model

--- src/main/java/com/sicredi/cooperativismo/service/TopicService.java
+++ src/main/java/com/sicredi/cooperativismo/service/TopicService.java
@@ -1,10 +1,13 @@
 package com.sicredi.cooperativismo.service;
 
+import com.sicredi.cooperativismo.domain.Meeting;
 import com.sicredi.cooperativismo.domain.Topic;
 import com.sicredi.cooperativismo.dto.request.TopicRequest;
 import com.sicredi.cooperativismo.exceptions.NotFoundException;
 import com.sicredi.cooperativismo.infra.ITopicRepository;
 import com.sicredi.cooperativismo.mapper.ITopicMapper;
+import com.sicredi.cooperativismo.validation.MeetingValidationService;
+import jakarta.validation.Validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +19,16 @@ public class TopicService implements ITopicService {
 
     private final ITopicMapper topicMapper;
 
+    private final IMeetingService meetingService;
+
     @Override
     public Topic createTopic(TopicRequest topicRequest) {
+        Meeting meeting = meetingService.getById(topicRequest.getMeetingId());
+        MeetingValidationService.validateMeetingStatus(meeting);
+
         Topic topic = topicMapper.topicRequestToTopic(topicRequest);
+        meeting.getTopics().add(topic);
+
         return this.topicRepository.save(topic);
     }
 
