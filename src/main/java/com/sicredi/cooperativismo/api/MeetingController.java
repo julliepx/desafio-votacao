2308a71d2f3a2079b0a4e6e1fc26c9e6d1555e82
Jullie <jlpaixaof@gmail.com>
2023-11-24 13:46:12 -0300

    feat: configurando documentação com Swagger e adicionando descrição dos endpoints

--- src/main/java/com/sicredi/cooperativismo/api/MeetingController.java
+++ src/main/java/com/sicredi/cooperativismo/api/MeetingController.java
@@ -3,6 +3,10 @@ package com.sicredi.cooperativismo.api;
 import com.sicredi.cooperativismo.domain.Meeting;
 import com.sicredi.cooperativismo.dto.request.MeetingRequest;
 import com.sicredi.cooperativismo.service.IMeetingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,21 +15,39 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/meetings")
+@Tag(name = "Assembleia")
 public class MeetingController {
 
     @Autowired
     private IMeetingService meetingService;
 
+    @Operation(summary = "Cria uma nova Assembleia")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Caso a Assembleia seja criada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Caso os dados informados sejam inválidos.")
+    })
     @PostMapping()
     public ResponseEntity<Meeting> createMeeting(@RequestBody @Valid MeetingRequest meetingRequest) {
         return new ResponseEntity<>(meetingService.createMeeting(meetingRequest), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Busca uma Assembleia pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Caso a Assembleia seja encontrada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Caso o ID informado seja inválido."),
+            @ApiResponse(responseCode = "404", description = "Caso não seja encontrada uma Assembleia com o ID informado.")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Meeting> getMeetingById(@PathVariable Long id) {
         return new ResponseEntity<>(meetingService.getById(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Encerra uma Assembleia")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Caso a Assembleia seja finalizada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Caso a Assembleia já foi encerrada ou o ID informado for inválido."),
+            @ApiResponse(responseCode = "404", description = "Caso não seja encontrada uma Assembleia com o ID informado.")
+    })
     @PatchMapping("/{id}")
     public ResponseEntity<Meeting> endMeeting(@PathVariable Long id) {
         meetingService.endMeeting(id);
--- src/main/java/com/sicredi/cooperativismo/api/VoteSessionController.java
+++ src/main/java/com/sicredi/cooperativismo/api/VoteSessionController.java
@@ -4,6 +4,10 @@ import com.sicredi.cooperativismo.domain.VoteSession;
 import com.sicredi.cooperativismo.dto.request.VoteSessionRequest;
 import com.sicredi.cooperativismo.dto.response.VoteSessionResultResponse;
 import com.sicredi.cooperativismo.service.IVoteSessionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,26 +16,50 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1/vote-sessions")
+@Tag(name = "Sessão de Votação")
 public class VoteSessionController {
 
     @Autowired
     private IVoteSessionService voteSessionService;
 
+    @Operation(summary = "Cria uma nova Sessão de Votação em uma Pauta")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Caso a Sessão seja criada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Caso os dados informados sejam inválidos.")
+    })
     @PostMapping()
     public ResponseEntity<VoteSession> createVoteSession(@RequestBody @Valid VoteSessionRequest voteSessionRequest) {
         return new ResponseEntity<>(this.voteSessionService.createVoteSession(voteSessionRequest), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Busca uma Sessão de Votação pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Caso a Sessão seja encontrada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Caso o ID informado seja inválido."),
+            @ApiResponse(responseCode = "404", description = "Caso não seja encontrada uma Sessão com o ID informado.")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<VoteSession> getVoteSessionById(@PathVariable Long id) {
         return new ResponseEntity<>(voteSessionService.getById(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Busca o resultado da Sessão de Votação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Caso o resultado da Sessão seja buscado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Caso o ID informado seja inválido."),
+            @ApiResponse(responseCode = "404", description = "Caso não seja encontrada uma Sessão com o ID informado.")
+    })
     @GetMapping("/result/{id}")
     public ResponseEntity<VoteSessionResultResponse> getVoteSessionResults(@PathVariable Long id) {
         return new ResponseEntity<>(voteSessionService.getVoteSessionResults(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Encerra uma Sessão de Votação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Caso a Sessão seja finalizada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Caso a Sessão já tenha sido encerrada ou o ID informado for inválido."),
+            @ApiResponse(responseCode = "404", description = "Caso não seja encontrada uma Sessão com o ID informado.")
+    })
     @PatchMapping("/end/{id}")
     public ResponseEntity<VoteSession> endVoteSession(@PathVariable Long id) {
         voteSessionService.endVoteSession(id);
