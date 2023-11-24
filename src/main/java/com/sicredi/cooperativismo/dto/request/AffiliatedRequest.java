b00dbccb12a14b807bf4c308bbcff22a51bff218
Jullie <jlpaixaof@gmail.com>
2023-11-24 11:02:51 -0300

    feat: complementing Affiliated context

--- src/main/java/com/sicredi/cooperativismo/dto/request/AffiliatedRequest.java
+++ src/main/java/com/sicredi/cooperativismo/dto/request/AffiliatedRequest.java
@@ -1,9 +1,11 @@
 package com.sicredi.cooperativismo.dto.request;
 
 import com.sicredi.cooperativismo.enums.AffiliatedStatusEnum;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AffiliatedRequest {
 
     private String name;
